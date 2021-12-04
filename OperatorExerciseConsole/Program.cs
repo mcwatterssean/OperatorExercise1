69cca207b0c624ba0ee34dce7982024d2fda3462
Sean McWatters <mcwatterssean@gmail.com>
2021-12-04 15:37:47 -0500

    initial commit

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+    int a = 17;
+    int b = 4;
+
+    int quotient = a / b;
+    int remainder = a % b;
+
+    if (a ==17 && b == 4)
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}" );
+
+
+}
+
+
+{ }
